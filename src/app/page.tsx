7492ecd038133647e9b368d0160f47dab56b52e9
Maxim <maxim.sukhodolets@gmail.com>
2023-07-30 18:18:36 +0300

    Added image lightbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,22 @@ import { Tab } from "@headlessui/react";
 import Link from "next/link";
 import Masonry from "react-masonry-css";
 import classNames from "classnames";
+import Image from "next/image";
+import type { LightGallery } from "lightgallery/lightgallery";
+import LightGalleryComponent from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
 
+import bgImage from "../../public/photographer.jpg";
 import landscape1 from "../../public/landscape-1.jpg";
 import landscape2 from "../../public/landscape-2.jpg";
 import landscape3 from "../../public/landscape-3.jpg";
 import landscape4 from "../../public/landscape-4.jpg";
 import landscape5 from "../../public/landscape-5.jpg";
-import Image from "next/image";
+import { useRef } from "react";
 
 const tabs = [
    {
@@ -26,16 +35,20 @@ const tabs = [
    },
 ];
 
+const images = [landscape1, landscape2, landscape3, landscape4, landscape5];
+
 export default function Home() {
+   const lightGallaryRef = useRef<LightGallery | null>(null);
+
    return (
-      <div className="h-full bg-[url('/photographer.jpg')] bg-top bg-cover overflow-auto">
-         <header className="bg-stone-900 fixed w-full z-10 flex justify-between items-center px-6 h-[90px]">
+      <div className="h-full overflow-auto">
+         <header className="fixed w-full z-10 flex justify-between items-center px-6 h-[90px]">
             <span className="uppercase py-3 font-medium">
                Photography portfolio
             </span>
             <Link
                href="#"
-               className="rounded-3xl bg-white text-stone-900 px-3 py-2 hover:bg-opacity-90"
+               className="rounded-3xl bg-stone-900 text-white px-3 py-2 hover:bg-opacity-90"
             >
                Get in touch
             </Link>
@@ -62,17 +75,34 @@ export default function Home() {
                   </Tab.List>
                   <Tab.Panels className="h-full max-w-[900px] w-full p-2 sm:p-4 my-6">
                      <Tab.Panel className="overflow-auto">
-                        <Masonry
-                           breakpointCols={2}
-                           className="flex gap-4"
-                           columnClassName=""
-                        >
-                           <Image src={landscape1} alt="1" className="my-4" />
-                           <Image src={landscape2} alt="2" className="my-4" />
-                           <Image src={landscape3} alt="3" className="my-4" />
-                           <Image src={landscape4} alt="4" className="my-4" />
-                           <Image src={landscape5} alt="5" className="my-4" />
+                        <Masonry breakpointCols={2} className="flex gap-4">
+                           {images.map((image, idx) => (
+                              <Image
+                                 key={image.src}
+                                 src={image}
+                                 placeholder="blur"
+                                 alt="landscape"
+                                 className="my-4 hover:opacity-90 cursor-pointer"
+                                 onClick={() =>
+                                    lightGallaryRef.current?.openGallery(idx)
+                                 }
+                              />
+                           ))}
                         </Masonry>
+                        <LightGalleryComponent
+                           onInit={(ref) => {
+                              if (ref) {
+                                 lightGallaryRef.current = ref.instance;
+                              }
+                           }}
+                           speed={500}
+                           plugins={[lgThumbnail, lgZoom]}
+                           dynamic
+                           dynamicEl={images.map((image) => ({
+                              src: image.src,
+                              thumb: image.src,
+                           }))}
+                        />
                      </Tab.Panel>
                      <Tab.Panel>Landscapes</Tab.Panel>
                      <Tab.Panel>People</Tab.Panel>
